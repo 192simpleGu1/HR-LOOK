ff61c93e61586ea299a457dbc5196bc6aefdb0df
6greenhand <2099769588@qq.com>
2024-04-03 18:15:30 +0800

    v2

---
+++ src/test/java/com/example/infusion/InfusionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.infusion;
+
+import cn.hutool.core.img.ImgUtil;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import com.example.infusion.domain.Bed;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
